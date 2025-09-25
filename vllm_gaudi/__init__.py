3a4bc3568471f1b8bfd37ef1740537cdfb49ab99
Mohit Deopujari <mdeopujari@habana.ai>
2025-09-25 19:20:21 +0000

    Overiding the _process_image_input for Gemma3

--- vllm_gaudi/__init__.py
+++ vllm_gaudi/__init__.py
@@ -22,3 +22,7 @@ def register_ops():
     import vllm_gaudi.ops.hpu_fp8  # noqa: F401
     import vllm_gaudi.ops.hpu_gptq  # noqa: F401
     import vllm_gaudi.ops.hpu_awq  # noqa: F401
+
+def register_models():
+    from .models import register_model
+    register_model()
